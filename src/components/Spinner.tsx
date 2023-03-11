f22b1ea4d5c3d6376dee32da5af4f30ef489072e
acolpitts <adam@colpitts.dev>
2023-03-11 10:23:32 -0400

    feat: add loading spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,108 @@
+export interface SpinnerProps {
+  size?: number;
+}
+
+export function Spinner({ size = 64 }: SpinnerProps) {
+  const colorPrimay = "var(--color-primary)";
+  const colorSecondary = "var(--color-secondary)";
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        margin: "auto",
+        background: "none",
+        display: "block",
+        shapeRendering: "auto",
+      }}
+      width={`${size}px`}
+      height={`${size}px`}
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <g>
+        <circle cx="60" cy="50" r="4" fill="var(--color-primary)">
+          <animate
+            attributeName="cx"
+            repeatCount="indefinite"
+            dur="1s"
+            values="95;35"
+            keyTimes="0;1"
+            begin="-0.67s"
+          ></animate>
+          <animate
+            attributeName="fill-opacity"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0;1;1"
+            keyTimes="0;0.2;1"
+            begin="-0.67s"
+          ></animate>
+        </circle>
+        <circle cx="60" cy="50" r="4" fill="var(--color-primary)">
+          <animate
+            attributeName="cx"
+            repeatCount="indefinite"
+            dur="1s"
+            values="95;35"
+            keyTimes="0;1"
+            begin="-0.33s"
+          ></animate>
+          <animate
+            attributeName="fill-opacity"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0;1;1"
+            keyTimes="0;0.2;1"
+            begin="-0.33s"
+          ></animate>
+        </circle>
+        <circle cx="60" cy="50" r="4" fill="var(--color-primary)">
+          <animate
+            attributeName="cx"
+            repeatCount="indefinite"
+            dur="1s"
+            values="95;35"
+            keyTimes="0;1"
+            begin="0s"
+          ></animate>
+          <animate
+            attributeName="fill-opacity"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0;1;1"
+            keyTimes="0;0.2;1"
+            begin="0s"
+          ></animate>
+        </circle>
+      </g>
+      <g transform="translate(-15 0)">
+        <path
+          d="M50 50L20 50A30 30 0 0 0 80 50Z"
+          fill="var(--color-primary)"
+          transform="rotate(90 50 50)"
+        ></path>
+        <path d="M50 50L20 50A30 30 0 0 0 80 50Z" fill="var(--color-secondary)">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0 50 50;45 50 50;0 50 50"
+            keyTimes="0;0.5;1"
+          ></animateTransform>
+        </path>
+        <path d="M50 50L20 50A30 30 0 0 1 80 50Z" fill="var(--color-secondary)">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0 50 50;-45 50 50;0 50 50"
+            keyTimes="0;0.5;1"
+          ></animateTransform>
+        </path>
+      </g>
+    </svg>
+  );
+}
